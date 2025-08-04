fb37c167f515ef7cbbd87fa80343d797bdc009e6
DonovanEarl <DonovanEarlFernandez@gmail.com>
2025-08-04 04:55:09 +0400

    Bug at Upd_person, see TODO

--- phonebook.py
+++ phonebook.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QAbstractItemView,QApplication,QWidget,QLineEdit,QPu
 from PyQt5.QtGui import QRegExpValidator
 from PyQt5 import Qt
 from PyQt5.QtCore import QRegExp,Qt
+import re
 
 
 #App Settings
@@ -24,6 +25,7 @@ text_boxnum.setDisabled(True)
 #text_boxnum.setValidator(QIntValidator()) # Downside is only 10 digits
 text_boxnum.setValidator(num_Validator)
 command={'value':None}
+field={'value:':None}
 
 #add Buttons
 add_button=QPushButton('Add')
@@ -137,19 +139,34 @@ def del_person():
     except:
         text_box.setPlaceholderText('Nothing to Delete!')
 
-def upd_person():
+def upd_person(): #TODO BUG: if clicked field is empty, nothing happens########################################
     
     session=Session()
+    print('REAACHED here')
     try:
-        upd_name=clicked_Field()
+        upd_name=clicked_Field
+        print('upd_name ni',upd_name)
         user=session.query(Person).filter_by(name=upd_name).first()
+        print('USERR:',user.name)
 
-        if user:
-            
-            
+        if user.name == upd_name:
+            print('DIRE ta:',user.name)
             edited_name=text_box.text()
-            edited_num=text_boxnum.text()
+            #edited_num=text_boxnum.text()
             user.name = edited_name
+            #user.number=edited_num
+
+            session.add(user)
+            session.commit()
+            show_all_person()
+        elif user.number==upd_name:
+            print('Nothing to update++')
+
+        else:
+            print('DIRE MO:',user.name)
+            #edited_name=text_box.text()
+            edited_num=text_boxnum.text()
+            #user.name = edited_name
             user.number=edited_num
 
             session.add(user)
@@ -178,36 +195,60 @@ def show_all_person():
 def clicked_Field():
     selected_item= list_table.selectedItems()
     print('Selected items',selected_item[0].text())
-    #print('Selected:',selected_item.text())
+    field['value']=selected_item[0].text()
     return selected_item[0].text()
 
-#-------Trial------work area #Idea# get command when button clicked and enable Textboxes for input, then commit command after Ok_button.clicked#
-def clicked_Button():
-    clicked_Button=app.sender().text()
-    text_box.setDisabled(False)
-    text_boxnum.setDisabled(False)
-    text_box.setPlaceholderText('Enter Name')
-    text_boxnum.setPlaceholderText('Enter Number')
-    ok_button.setDisabled(False)
-    print('Clicke_Button:',clicked_Button)
-    #if clicked_Button=='add' or 'Add':
-        #print('You clicked the add button')
-        #clicked_Button_text='add'
-        #print('Clicked_text:',clicked_Button_text)
+#function to get the text value of the button being pressed, use it for command when pressing Ok_button#
+def clicked_Button(): #FIXED
+    
+    #print('Field Value:',field['value'])
+    field_v=field['value']
+    field_v=str(field_v)
+    if re.search(r"[a-z,A-Z]",field_v):
         
-    command['value']=str(clicked_Button).lower()  #Used a dictionary to forward text value to another function clicked_commitButton#
+        clicked_Button=app.sender().text()
+        text_box.setDisabled(False)
+        #text_boxnum.setDisabled(False)
+        text_box.setPlaceholderText('Enter Name')
+        #text_boxnum.setPlaceholderText('Enter Number')
+        ok_button.setDisabled(False)
+        print('Clicked Button:',clicked_Button)
+        command['value']=str(clicked_Button).lower()  #Used a dictionary to forward text value to another function clicked_commitButton#
+        print('command',command['value'])
+    else:
+        clicked_Button=app.sender().text()
+        #text_box.setDisabled(False)
+        text_boxnum.setDisabled(False)
+        #text_box.setPlaceholderText('Enter Name')
+        text_boxnum.setPlaceholderText('Enter Number')
+        ok_button.setDisabled(False)
+        print('Clicked Button:',clicked_Button)
+        command['value']=str(clicked_Button).lower()  #Used a dictionary to forward text value to another function clicked_commitButton#
+        print('commandSTR',command['value'])
 
-
-#Todo : Reduce redundant button clicks to get the value
+#function for the Ok_button#
 def clicked_commitButton():  
     command_text=command['value']
-    print('Now here:',command_text)
+   
     if command_text=='add':
         new_person()
+        text_box.setDisabled(True)
+        text_boxnum.setDisabled(True)
+        text_box.setPlaceholderText('')
+        text_boxnum.setPlaceholderText('')
     elif command_text=='del':
         del_person()
     elif command_text=='edit':
+        print('Reached++++')
         upd_person()
+        text_box.setDisabled(True)
+        text_boxnum.setDisabled(True)
+        text_box.clear()
+        text_boxnum.clear()
+        text_box.setPlaceholderText('')
+        text_boxnum.setPlaceholderText('')
+    else:
+        print('Nothing pressed')
 
 ## Start executing
 
