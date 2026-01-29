7048bcd4d9efa69b70ef6d6ec7d41d56133b061b
DonovanEarl <DonovanEarlFernandez@gmail.com>
2026-01-29 21:06:18 +0400

    Added hover effect for all media queries,adjusted navbar buttonssizes

--- dj_project/frontend/src/pages/Home.jsx
+++ dj_project/frontend/src/pages/Home.jsx
@@ -9,7 +9,9 @@ import LandingPageContents from "../components/LandingPageContents"
 function Home(){
     let head1="Computer & IT Support Made Easy"
     let head2="Fast. Reliable. Right at Your Doorstep."
-    let par1="Slow laptop? PC won't turn on? Need upgrades or IT support?Pinoy-Tech provides computer repair and IT services anywhere in Fujairah—so you don't have to go out to find help. Serving Fujairah since 2016, we deliver affordable, dependable solutions backed by our No Fix - No Pay guarantee."
+    let par1="Slow laptop? PC won't turn on? Need upgrades or IT support?"
+    let par2="Pinoy-Tech provides computer repair and IT services anywhere in Fujairah—so you don't have to go out to find help."
+    let par3="Serving Fujairah since 2016, we deliver affordable, dependable solutions backed by our No Fix - No Pay guarantee."
     let btn_book="👉 Book Your Service Today"
     return (
         
@@ -24,8 +26,11 @@ function Home(){
                     {head2}
                 </h2>
                 <p>
-                    {par1}
+                    {par1}<br/>
+                    {par2}
+                    {par3}
                 </p>
+               
                 <div className="cta-cards">
                     <button className="contact-button">👉 Get Your Computer Fixed Today</button>
                     <LandingPageContents/>
