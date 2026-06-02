ab54b6f79e8c0790f512a8648c64b4279ad515e8
DonovanEarl <DonovanEarlFernandez@gmail.com>
2026-06-02 19:46:01 +0400

    Comment out Staticfiles whitenoise

--- dj_project/dj_project/settings.py
+++ dj_project/dj_project/settings.py
@@ -173,9 +173,9 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
+    # "staticfiles": {
+    #     "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    # },
 }
 
 MEDIA_URL = 'media/'
