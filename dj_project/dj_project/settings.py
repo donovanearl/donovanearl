e4012ecf2b3b5e4c50dd70992caaa49266fb48fa
DonovanEarl <DonovanEarlFernandez@gmail.com>
2026-06-10 19:06:22 +0800

    settings.py access token to 60mins

--- dj_project/dj_project/settings.py
+++ dj_project/dj_project/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=45),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 } #added
 
