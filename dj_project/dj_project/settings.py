2ea5b5c89b3c88428bfffc9454a3802a6d2da230
DonovanEarl <DonovanEarlFernandez@gmail.com>
2026-09-18 02:06:23 +0400

    replaced console log for email with real smtp from google for self notify

--- dj_project/dj_project/settings.py
+++ dj_project/dj_project/settings.py
@@ -215,10 +215,16 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
 ]
 
-# --------Emails---------
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "PinoyTech.ae@gmail.com"
-NOTIFY_EMAIL = "PinoyTech.ae@gmail.com"
+# --------Email to self notify---------
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
+NOTIFY_EMAIL = os.getenv("EMAIL_HOST_USER")
+
 SITE_URL = "https://www.pinoy-tech.com"
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
